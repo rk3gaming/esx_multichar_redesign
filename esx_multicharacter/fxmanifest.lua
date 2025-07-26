29c5eb1a6fc6bee05934216dd2a8c3b41b4aa954
RK <rkfrmda3@gmail.com>
2025-07-25 21:35:34 -0500

    Update fxmanifest.lua

--- esx_multicharacter/fxmanifest.lua
+++ esx_multicharacter/fxmanifest.lua
@@ -6,7 +6,7 @@ lua54 'yes'
 
 dependencies { 'es_extended', 'esx_identity' }
 
-shared_scripts { '@es_extended/imports.lua', '@es_extended/shared/utils/locale.lua', 'locales/*.lua', 'shared/*.lua', 'config.lua' }
+shared_scripts { '@es_extended/imports.lua', '@es_extended/locale.lua', 'locales/*.lua', 'shared/*.lua', 'config.lua' }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
